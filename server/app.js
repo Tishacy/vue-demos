17f4e178745860e359a6b04f09a019dc7df3d413
Tishacy <Tishacy@gmail.com>
2020-01-26 22:40:26 +0800

    add simple server.

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express()
+const port = 3000
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const response = await fetch("https://www.zhihu.com/api/v4/questions/367753181/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=20&offset=0&platform=desktop&sort_by=default")
+    const jsonData = await response.json();
+    const resData = [];
+    jsonData.data.forEach((item, index) => {
+        resData.push({
+            "title": item.excerpt,
+            "author": item.author.name,
+            "date": item.updated_time + '000'
+        })
+    })
+    res.json(resData);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
